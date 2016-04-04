80b53a347ba60e0611eae3712fd2505c042575a4
zlargon <zlargon@icloud.com>
2016-04-04 21:55:54 +0800

    update module 'normalize.css' to 4.0.0

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import 'normalize.css';
+import 'normalize.css/normalize.css'; // v4.0.0: missing `main` in package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
